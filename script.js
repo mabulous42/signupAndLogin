68f9d8bf38c780f192415fe05b4d2d8f6e57ba2c
Abbas <tundeeey@gmail.com>
2023-04-11 22:03:35 +0100

    was able to save each users details from the signUp page as an object and pushed into an array of objects

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.getElementById("login-box").style.display = "none";
 let loading = document.getElementById("loading-div");
-loadingL = document.getElementById("loading-divL");
+let loadingL = document.getElementById("loading-divL");
+let fullName = document.getElementById('f-name');
+let email = document.getElementById('user-email');
+let signUpMessage = document.getElementById('signup-msg');
+let password = document.getElementById('user-password');
+let confirmPassword = document.getElementById('confirm-password');
 
 loading.style.display = "none";
 loadingL.style.display = "none";
 
 function loginPage() {
-    
+
     document.getElementById("sign-up-box").style.display = "none";
     document.getElementById("login-box").style.display = "block";
 }
@@ -18,15 +23,51 @@ function signUpPage() {
 
 }
 
+let myObj;
+
+
+let registeredUser = []
+
 function signUp() {
-    loadingL.style.display = "block";
-    document.getElementById("sign-up-box").style.display = "block";
-    document.getElementById("login-box").style.display = "none";
-    setTimeout(() => {
-        // document.getElementById("sign-up-box").style.display = "block";
-        // document.getElementById("login-box").style.display = "none";
-        loadingL.style.display = "none";
-    }, 3000);
+    if (password.value === confirmPassword.value && fullName.value != "" && email.value != "") {
+        
+        myObj = {
+            fullname: fullName.value,
+            email: email.value,
+            password: password.value,
+            confirmPassword: confirmPassword.value
+        }
+        // console.log(myObj);
+        // registeredUser = [myObj];
+        // console.log(registeredUser);
+        
+        registeredUser.push(myObj); //Add the text 'item1' to registeredUser
+        localStorage.setItem('member', JSON.stringify(registeredUser));
+        signUpMessage.innerHTML = `
+        <p id="success-msg">Sign up successful!!!</p>
+        `
+        loading.style.display = "block";
+        setTimeout(() => {
+            document.getElementById("sign-up-box").style.display = "none";
+            document.getElementById("login-box").style.display = "block";
+            loading.style.display = "none";
+            signUpMessage.style.display = 'none';
+        }, 3000);
+
+        fullName.value = "";
+        email.value = "";
+        password.value = "";
+        confirmPassword.value = "";
+    } else {
+        signUpMessage.innerHTML = `
+        <p id="failed-msg">Sign up failed!!!</p>
+        `
+        setTimeout(() => {
+            signUpMessage.style.display = 'none';
+        }, 3000);
+    }
+
+
 }
 
 function login() {
@@ -38,3 +79,12 @@ function login() {
 
     }, 3000);
 }
+
+let getter = "";
+// function getUserInfo() {
+//    getter = JSON.parse(localStorage.getItem('member'));
+//    console.log(getter);
+// }
+// getUserInfo();
+
+
