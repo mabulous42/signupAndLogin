9b50dd9ba5e72ecf2c01bfd923474148242886e0
Abbas <tundeeey@gmail.com>
2023-04-11 11:51:10 +0100

    I removed the loading animation from the signup and login small button, I want to move it to the big signup and login button, that should after the user has already entered his or her details under the signup page and login page respectively

--- script.js
+++ script.js
@@ -5,23 +5,34 @@ loadingL = document.getElementById("loading-divL");
 loading.style.display = "none";
 loadingL.style.display = "none";
 
-function login() {
-    loading.style.display = "block";
-    setTimeout(() => {
-        document.getElementById("sign-up-box").style.display = "none";
-        document.getElementById("login-box").style.display = "block";
-        loading.style.display = "none";
-
-    }, 3000);
+function loginPage() {
+    
+    document.getElementById("sign-up-box").style.display = "none";
+    document.getElementById("login-box").style.display = "block";
 }
 
+
+function signUpPage() {
+    document.getElementById("sign-up-box").style.display = "block";
+    document.getElementById("login-box").style.display = "none";
+
+}
 
 function signUp() {
     loadingL.style.display = "block";
     setTimeout(() => {
-        document.getElementById("sign-up-box").style.display = "block";
-        document.getElementById("login-box").style.display = "none";
+        // document.getElementById("sign-up-box").style.display = "block";
+        // document.getElementById("login-box").style.display = "none";
         loadingL.style.display = "none";
     }, 3000);
+}
 
+function login() {
+    loading.style.display = "block";
+    setTimeout(() => {
+        // document.getElementById("sign-up-box").style.display = "none";
+        // document.getElementById("login-box").style.display = "block";
+        loading.style.display = "none";
+
+    }, 3000);
 }
