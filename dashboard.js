03e9e69402d0d896ad6daad01dcde8509f9bf349
Abbas <tundeeey@gmail.com>
2023-04-15 09:43:02 +0100

    so far, I was able to add the like button to the displayed post, and also been able to display all the liked post in the view liked post session, still working on the like button to change to unlike on each click and change back to like if clicked again, while the like button is clicked, the post is unshift(added to the top of the array), so it has to be removed from the array if the unlike button is clicked

--- dashboard.js
+++ dashboard.js
@@ -3,9 +3,11 @@ let createPostSection = document.getElementById('publish-post');
 let postTitle = document.getElementById('post-title');
 let postContent = document.getElementById('post-content');
 let viewPosts = document.getElementById('homepage');
+let viewLikedPost = document.getElementById('view-liked-post');
 
 
 createPostSection.style.display = 'none';
+viewLikedPost.style.display = 'none';
 
 let loggedInUser = JSON.parse(localStorage.getItem("CU"));
 console.log(loggedInUser);
@@ -42,33 +44,75 @@ function publishPost() {
 
     if (displayPost == null) {
         displayPost = [];
-        displayPost.push(myBlog);
+        displayPost.unshift(myBlog);
         localStorage.setItem("Post", JSON.stringify(displayPost));
     } else {
-        displayPost.push(myBlog);
+        displayPost.unshift(myBlog);
         localStorage.setItem("Post", JSON.stringify(displayPost));
     }
-
+    displayMyContent(displayPost, viewPosts);
 }
 
-function showAllPosts() {
-    createPostSection.style.display = 'none';
-    displayPost.forEach((element, index) => {
-        viewPosts.innerHTML += `
+function displayMyContent(arrayName, theHTMLTag) {
+    theHTMLTag.innerHTML = "";
+    arrayName.forEach((element, index) => {
+        theHTMLTag.innerHTML += `
         <div class='post-view-div mb-4'>
             <h3 class='title'>${element.title}</h3>
             <p class='my-content'>${element.content}</p>
             <p>Author: ${element.author}</p>
             <p>Time: ${element.time}</p>
-            <button id='remove' class="btn btn-primary" type="submit" onclick="del(${index})">Delete Post</button>
+            <button id='remove' class="remove btn btn-primary" type="submit" onclick="del(${index})">Delete Post</button>
+            <button id='like' class="remove btn btn-primary" type="submit" onclick="like(${index})">Like</button>
         </div>
         `
     });
+}
+
+displayMyContent(displayPost, viewPosts);
+
+function showAllPosts() {
+    createPostSection.style.display = 'none';
+    viewLikedPost.style.display = 'none';
+    document.getElementById('vp').innerHTML = "All Posts"
+    // displayMyContent(displayPost, viewPosts);
+    
+}
+
+let saveLikedPost = JSON.parse(localStorage.getItem("liked"));
+let likeBtn = document.getElementById('like');
+
+function like(index) {
+    console.log(likeBtn.innerHTML);
+    if (likeBtn.innerHTML === "Like") {
+        likeBtn.innerHTML = "Unlike";
+        if (saveLikedPost == null) {
+            saveLikedPost = [];
+            saveLikedPost.unshift(displayPost[index]);
+            localStorage.setItem("liked", JSON.stringify(saveLikedPost));
+        } else {
+            saveLikedPost.unshift(displayPost[index]);
+            localStorage.setItem("liked", JSON.stringify(saveLikedPost));
+        }
+    } else {
+        likeBtn.innerHTML = "Like";
+        
+    }
+    
+}
+
+function viewLiked() {
+    viewLikedPost.style.display = 'block';
+    createPostSection.style.display = 'none';
+    document.getElementById('vp').innerHTML = "View Posts";
+    displayMyContent(saveLikedPost, viewLikedPost);
 
 }
 
 function createPost() {
     createPostSection.style.display = 'block';
+    viewLikedPost.style.display = 'none';
+    document.getElementById('vp').innerHTML = "View Posts"
 
 }
 
@@ -76,11 +120,30 @@ function del(index) {
     viewPosts.innerHTML = "";
     displayPost.splice(index, 1)
     localStorage.setItem("Post", JSON.stringify(displayPost));
-    showAllPosts();
+    displayMyContent(displayPost, viewPosts);
 }
 
-let delButton = document.getElementById('remove');
-if (!loggedInUser) {
-    
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let delButton = document.getElementsByClassName('remove');
+
+
+// let v = document.getElementById("v");
+// v.disabled = true;
 
