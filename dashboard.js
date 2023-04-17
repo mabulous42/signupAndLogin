49e815a98e9217af680b893eaf4170a2cd8aa05b
Abbas <tundeeey@gmail.com>
2023-04-17 21:12:35 +0100

    done with displaying all like post under viewLikedPost, remain to work on displaying the number of likes each post has

--- dashboard.js
+++ dashboard.js
@@ -8,8 +8,10 @@ let viewLikedPost = document.getElementById('view-liked-post');
 
 //displaying the create Post section off until it is needed
 createPostSection.style.display = 'none';
+
 //displaying the liked post section off until it is needed
 viewLikedPost.style.display = 'none';
+
 
 //getting the Current user details and saving it in as array of loggedInUser
 let loggedInUser = JSON.parse(localStorage.getItem("CU"));
@@ -22,7 +24,7 @@ console.log(loggedInUser);
 if (!loggedInUser) {
     alert("You have to sign in before you can access this page, you will be redirected back to login page");
     window.location.href = "index.html";
-}else{
+} else {
     userName.innerHTML = loggedInUser.fullname;
 }
 
@@ -38,11 +40,16 @@ let date = new Date();
 //getting all the posts from the users and saving it in the local storage displayPost
 let displayPost = JSON.parse(localStorage.getItem("Post"));
 
+if (displayPost == null) {
+    displayPost = [];
+}
+console.log(displayPost.length);
+
 //saving all the posts inside the localstorage and displaying them
 function publishPost() {
     createPostSection.style.display = 'none';
     myBlog = {
-        id: displayPost.length+1,
+        id: displayPost.length + 1,
         title: postTitle.value,
         content: postContent.value,
         author: loggedInUser.fullname,
@@ -64,7 +71,7 @@ function publishPost() {
 //this is a global function that handles display of post by being called by other fuctions that might need it
 function displayMyContent(arrayName, theHTMLTag) {
     theHTMLTag.innerHTML = "";
-    if (displayPost == null) {
+    if (arrayName == null) {
         return;
     } else {
         arrayName.forEach((element, index) => {
@@ -97,7 +104,6 @@ function displayMyContent(arrayName, theHTMLTag) {
             }
         });
     }
-    
 }
 
 //calling the global function to display all the contents in the displayPost array
@@ -107,87 +113,37 @@ displayMyContent(displayPost, viewPosts);
 function showAllPosts() {
     createPostSection.style.display = 'none';
     viewLikedPost.style.display = 'none';
-    document.getElementById('vp').innerHTML = "All Posts"
-    // displayMyContent(displayPost, viewPosts);
-
+    document.getElementById('vp').innerHTML = "All Posts";
 }
-
-//getting all liked post and saving inside the saveLikedPost array
-let saveLikedPost = JSON.parse(localStorage.getItem("liked"));
-
-//getting element by id of the like and unlike buttons
-let likeBtn = document.getElementById('like');
-let unlikeBtn = document.getElementById('unlike');
 
 
 //the like function when clicked unshift the post to the top of the array
-function like(id){
-    let found = displayPost.find(el=> el.id == id);
-    if(found.isLiked == false){
+function like(id) {
+    let found = displayPost.find(el => el.id == id);
+    if (found.isLiked == false) {
         found.isLiked = true;
         localStorage.setItem("Post", JSON.stringify(displayPost))
         displayMyContent(displayPost, viewPosts);
-    }else{
+    } else {
         found.isLiked = false;
         localStorage.setItem("Post", JSON.stringify(displayPost))
         displayMyContent(displayPost, viewPosts);
     }
     console.log(found);
 }
-// function like(index) {
-//     console.log(likeBtn.value);
-//     if (likeBtn.value === "Like") {
-//         // likeBtn.value = "Unlike";
-//         // likeBtn.style.display = "none";
-//         // unlikeBtn.style.display = "block";
-//         if (saveLikedPost == null) {
-//             saveLikedPost = [];
-//             saveLikedPost.unshift(displayPost[index]);
-//             localStorage.setItem("liked", JSON.stringify(saveLikedPost));
-//             // viewLiked();
-//         } else {
-//             saveLikedPost.unshift(displayPost[index]);
-//             localStorage.setItem("liked", JSON.stringify(saveLikedPost));
-//             // viewLiked();
-//         }
-//     } else {
-//         likeBtn.innerHTML = "Like";
-//         saveLikedPost.splice(index, 1);
-//         localStorage.setItem("liked", JSON.stringify(saveLikedPost));
-//     }
 
-// }
+//getting all liked post and saving inside the saveLikedPost array
+let saveLikedPost = JSON.parse(localStorage.getItem("Post"));
 
-// function unlike(index) {
-//     // if (likeBtn.value === "Unlike") {
-//     //     likeBtn.value = "Like"
-//     // } else {
-//     //     likeBtn.value = "Unlike"
-//     // }
-//     removeItems(viewLikedPost, saveLikedPost, index);
-//     localStorage.setItem("liked", JSON.stringify(saveLikedPost));
-//     viewLiked();
-// }
 
 //displaying all the liked posts
 function viewLiked() {
     viewLikedPost.style.display = 'block';
     createPostSection.style.display = 'none';
-    document.getElementById('vp').innerHTML = "View Posts";
-    // displayMyContent(saveLikedPost, viewLikedPost);
-
-    viewLikedPost.innerHTML = `<h1>Liked Posts</h1>`;
-    saveLikedPost.forEach((ele, index) => {
-        viewLikedPost.innerHTML += `
-        <div class='post-view-div mb-4'>
-            <h3 class='title'>${ele.title}</h3>
-            <p class='my-content'>${ele.content}</p>
-            <p>Author: ${ele.author}</p>
-            <p>Time: ${ele.time}</p>
-            <button id='like' class="remove btn btn-primary" type="submit" onclick="unlike(${index})">Unlike</button>
-        </div>
-        `
-    });
+    
+    saveLikedPost = displayPost.filter(el => el.isLiked == true);
+    console.log(saveLikedPost);
+    displayMyContent(saveLikedPost, viewLikedPost);
 }
 
 //this function allow users to creates a new post
@@ -195,58 +151,20 @@ function createPost() {
     createPostSection.style.display = 'block';
     viewLikedPost.style.display = 'none';
     document.getElementById('vp').innerHTML = "View Posts"
-
 }
 
 //a global function that deletes post from the array through it's index
-function removeItems(theHTMLTag, arrayElement, index) {
-    theHTMLTag.innerHTML = "";
-    arrayElement.splice(index, 1)
-}
-
-//this function deletes from all posts by calling the removeItems which is the global function
-function del(index) {
-    displayPost.forEach((ele) => {
-        let foundInLikePost = saveLikedPost.find((element) => element.content === ele.content);
-        // console.log(foundInLikePost);
-
-        if (foundInLikePost) {
-            removeItems(viewPosts, displayPost, index);
-            localStorage.setItem("Post", JSON.stringify(displayPost));
-            displayMyContent(displayPost, viewPosts);
-
-            //calling the global delete function
-            removeItems(viewLikedPost, saveLikedPost, index);
-            localStorage.setItem("liked", JSON.stringify(saveLikedPost));
-            // viewLiked();
-        } else {
-            removeItems(viewPosts, displayPost, index);
-            localStorage.setItem("Post", JSON.stringify(displayPost));
-            displayMyContent(displayPost, viewPosts);
-        }
-    })
-
-    // console.log(foundInLikePost);
-
-}
-let v = document.getElementById("vp");
-let delButton = document.getElementById('remove');
-// if (!loggedInUser) {
-//     delButton.disabled = true;
-//     // v.disabled = true;
+// function removeItems(theHTMLTag, arrayElement, index) {
+//     theHTMLTag.innerHTML = "";
+//     arrayElement.splice(index, 1)
 // }
 
-console.log(loggedInUser.fullname);
 
-// displayPost.forEach(element => {    
-//     console.log(element.author);
-
-//     if (loggedInUser.fullname === element.author) {
-//         delButton.disabled = false;
-//     } else {
-//         delButton.disabled = true;
-//     }
-
-//     console.log(loggedInUser.fullname !== element.author);
-// })
+//this function deletes from all posts
+function del(index) {
+    viewPosts.innerHTML = "";
+    displayPost.splice(index, 1)
+    localStorage.setItem("Post", JSON.stringify(displayPost));
+    displayMyContent(displayPost, viewPosts);
+}
 
